0f67f58a1aef28f311f7ab2fed169070ec1191c4
dhruv gupta <dgastir69@gmail.com>
2025-08-05 16:02:10 +0530

    wrote the logic for single round

--- scripts.js
+++ scripts.js
@@ -1,13 +1,9 @@
-let choices = ["rock","paper","scissors"];
 
 function getComputerChoice(){
+    let choices = ["rock","paper","scissors"];
     let randomindex = Math.floor(Math.random()*choices.length);
     return choices[randomindex];
 }
-
-console.log(getComputerChoice());
-
-
 
 function getHumanChoice(string){
     user = prompt("lets play rock,paper,or scissors");
@@ -22,4 +18,27 @@ function getHumanChoice(string){
 
     }
 }
-console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return("It's a Tie!");
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        return("You Win!");
+    } else {
+        return("Computer Wins!");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log(playRound(humanSelection,computerSelection));
+
